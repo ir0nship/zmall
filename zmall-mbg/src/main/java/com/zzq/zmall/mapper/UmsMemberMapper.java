bab9688401e4fe594a276bee955f246a0f5d2491
ironship <1021368421@qq.com>
2022-11-07 16:50:45 +0800

    mbg

---
+++ zmall-mbg/src/main/java/com/zzq/zmall/mapper/UmsMemberMapper.java
@@ -0,0 +1,30 @@
+package com.zzq.zmall.mapper;
+
+import com.zzq.zmall.model.UmsMember;
+import com.zzq.zmall.model.UmsMemberExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsMemberMapper {
+    long countByExample(UmsMemberExample example);
+
+    int deleteByExample(UmsMemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsMember row);
+
+    int insertSelective(UmsMember row);
+
+    List<UmsMember> selectByExample(UmsMemberExample example);
+
+    UmsMember selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") UmsMember row, @Param("example") UmsMemberExample example);
+
+    int updateByExample(@Param("row") UmsMember row, @Param("example") UmsMemberExample example);
+
+    int updateByPrimaryKeySelective(UmsMember row);
+
+    int updateByPrimaryKey(UmsMember row);
+}
---
+++ zmall-mbg/src/main/java/com/zzq/zmall/mapper/UmsPermissionMapper.java
@@ -0,0 +1,30 @@
+package com.zzq.zmall.mapper;
+
+import com.zzq.zmall.model.UmsPermission;
+import com.zzq.zmall.model.UmsPermissionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsPermissionMapper {
+    long countByExample(UmsPermissionExample example);
+
+    int deleteByExample(UmsPermissionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsPermission row);
+
+    int insertSelective(UmsPermission row);
+
+    List<UmsPermission> selectByExample(UmsPermissionExample example);
+
+    UmsPermission selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") UmsPermission row, @Param("example") UmsPermissionExample example);
+
+    int updateByExample(@Param("row") UmsPermission row, @Param("example") UmsPermissionExample example);
+
+    int updateByPrimaryKeySelective(UmsPermission row);
+
+    int updateByPrimaryKey(UmsPermission row);
+}
